52bdf1a8fd5d74a93baafa4f5c4291d6b5a74585
toneloke <david.guerrero87@live.com>
2019-04-15 00:48:41 -0700

    renamed index to server

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(`${base}/questions`, questionRoutes);
 //production mode check
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendfile(path.join(__dirname = '/client/build/index.html'));
   })
 }else{
